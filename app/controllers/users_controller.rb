0a8b88436fafa03321b513bb90c88e6dcda355a3
Amaralingeswara Rao Thondapu <amarshiva93@gmail.com>
2023-11-06 12:06:35 +0530

    added pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def new
@@ -17,7 +21,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information updated successfully."
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit', status: :unprocessable_entity
         end
@@ -28,8 +32,8 @@ class UsersController < ApplicationController
         # @article.user = User.first
         if @user.save
             flash[:notice] = "Welcome to Amar Blog #{@user.username}, You have successfully signed up"
-            redirect_to articles_path
-            # redirect_to @user
+            # redirect_to articles_path
+            redirect_to @user
         else
             render 'new', status: :unprocessable_entity
         end
