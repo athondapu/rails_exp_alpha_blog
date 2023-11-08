36fea812de7144bd86942a82045001f04b6bb5d2
Amaralingeswara Rao Thondapu <amarshiva93@gmail.com>
2023-11-08 20:01:45 +0530

    Added unit and integration test cases

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @admin_user = User.create(username: 'john', email: 'john@gmail.com', 
+                              password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end
+
+  test "get new category form and create category" do
+    get '/categories/new'
+    # get new_category_path
+    assert_response :success
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: {category: {name: "Sports" } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "Sports", response.body
+  end
+
+  test "get new category form and reject invalid category submission" do
+    get '/categories/new'
+    # get new_category_path
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: "" } }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+
+  end
 end
